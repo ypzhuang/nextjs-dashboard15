b6d9ca139e0d8c1d1b9e79b0152e9b8591c8cc43
John Zhuang <zhuangyinping@gmail.com>
2024-12-11 13:50:44 +0800

    before db

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,6 +2,8 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
+// ORM: https://www.prisma.io/
+// https://orm.drizzle.team/
 export type User = {
   id: string;
   name: string;
